03d425cfc5cefa0dec300066965b42352bbd56c7
PkSM3 <pokesam3@gmail.com>
2014-04-15 02:12:32 -0300

    sigma 1.0 example

--- newComex/genesis.js
+++ newComex/genesis.js
@@ -1,2 +1,39 @@
+function sigmaLimits(){
+	sidebar=$('#leftcolumn').width();
+	anchototal=$('#fixedtop').width();
+	altototal=$('#leftcolumn').height();
+	altofixtop=$('#fixedtop').height()
+	altodeftop=$('#defaultop').height()
+	$('#sigma-example').width(anchototal-sidebar);
+	$('#sigma-example').height(altototal-altofixtop-altodeftop-2);
+}
+
 var body=document.getElementsByTagName('body')[0];
 body.style.paddingTop="41px";
+
+sigmaLimits();
+
+$( window ).resize(function() {
+  sigmaLimits();
+});
+
+partialGraph={}
+
+sigma.parsers.gexf('data/miserables.gexf', {
+	container: 'sigma-example',
+	settings: {
+		drawEdges: false,
+		labelThreshold:5
+	}
+},
+function(s) {
+	console.log(s)
+	var c = s.camera;
+	c.goTo({ ratio: 1.2 });
+	s.startForceAtlas2();
+});
+
+
+
+
+
